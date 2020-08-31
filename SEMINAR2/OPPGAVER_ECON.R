a087304953b9e075498248130d09d72f6a93a1d4
Louisa Boulaziz <54175610+louisabo@users.noreply.github.com>
2020-08-31 16:23:46 +0200

    Legger ut seminar oppgaver til etter andre seminar

---
+++ SEMINAR2/OPPGAVER_ECON.R
@@ -0,0 +1,41 @@
+
+#### OPPGAVER TIL ETTER SEMINARET 
+
+# 1. Plot inntekt som Y og utdanning som X og  
+# sorter observasjonene vha color = gender 
+
+# 2. Plot inntekt som Y og utdanning X. Lag en 
+# regresjonslinje for menn og en for kvinner 
+
+# 3. Hvor mange kvinner tjener over 30.000? 
+# 4. Hvor mange menn tjener over 30.000? 
+
+# 5. Lag en OLS med Y som inntekt, 
+# og alder og kjønn som uavhengige variable 
+
+# 6. Plot modellen din 
+
+# 7. Lag en ny variabel hvor du slår sammen 
+# utdanningsnivåene 2+3 (bachelor + master) til 1 
+# og 1 til 0 (high school) 
+
+# 8. Lag en ytterligere variabel hvor utdanningsnivået 
+# 1 i den forrige variabelen heter "university education"
+# mens 0 er "no university education"
+
+# 9. Hva er gjennomsnittsalderen til menn og kvinner?
+# hva er forskjellen?
+
+# 10. Lag et nytt datasett med observasjoner som 
+# har en høyere alder en gjennosnittet 
+
+# 11. Estimer gjennomsnittsalderen i det nye datasettet
+
+# 12. Sentrer variabelen alder 
+
+# 13. Ta utgangspunkt i variabelen inntekt 
+# Finn gjennomsnittet 
+# Bruk gjennomsnittet til å dele variabelen i to 
+# Du skal opprette en ny variabel hvor personer som har
+# inntekt som er lavere enn gjennomsnittet får verdien 0
+# og personer som har inntekt over gjennomsnittet får verdien 1
