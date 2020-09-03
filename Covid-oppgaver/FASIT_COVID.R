d407f3bb3852fcf1834a803275dc1c6c13f353f2
Louisa Boulaziz <54175610+louisabo@users.noreply.github.com>
2020-09-03 17:02:39 +0200

    Legger ut ekstra oppgaver

---
+++ Covid-oppgaver/FASIT_COVID.R
@@ -0,0 +1,75 @@
+#Løsningsforslag COVID19 OPPGAVER 
+
+covidata <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-16-2020.csv?fbclid=IwAR1Snt_p5Ir0pmWKIHAO2grXrGI4uk0s7_oUetEi8zOaWoT7g134HiG0zeY",
+                     stringsAsFactors = FALSE)
+
+data <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/09-01-2020.csv", 
+                 stringsAsFactors = F)
+library(tidyverse)
+View(data)
+
+## Oppgave 1:
+
+ddata <- subset(data, Deaths > 0)
+
+#92 observasjoner. Men mange av dem er forskjellige stater i 
+#samme land. Vi kan bruke table() for ? se hvilke unike land:
+table(ddata$Country.Region)
+
+#hvor mange er det?
+length(table(ddata$Country_Region))
+#175
+
+#eller bruk funskjonen unique():
+unique(ddata$Country_Region)
+
+## Oppgave 2:
+
+kdata <- subset(data, Country_Region == "China")
+#her er hver rad en provins
+nrow(kdata) # = 33
+
+## Oppgave 3:
+
+ldata <- subset(data, Deaths == 0)
+mean(ldata$Long_, na.rm = T) # = -86
+
+## Oppgave 4:
+
+data <- data %>%
+  mutate(rec_frac = Recovered / Confirmed,
+         death_frac = Deaths / Confirmed,
+         sick_frac = (Confirmed - (Recovered + Deaths)) / Confirmed)
+
+#evt:    sick_frac = 1 - (rec_frac + death_frac)
+
+## Oppgave 5:
+
+ggplot(data, aes(x = rec_frac)) + 
+  geom_density()
+
+ggplot(data, aes(x = death_frac)) + 
+  geom_density()
+
+ggplot(data, aes(x = sick_frac)) + 
+  geom_density()
+
+## Oppgave 6:
+data$dist_eq <- abs(data$Lat)
+mean(data$dist_eq, na.rm = T) # = 37
+
+## Oppgave 7:
+
+ggplot(data, aes(x = dist_eq, y = death_frac)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+
+## Oppgave 8:
+
+data$Province_State <- ifelse(data$Province_State == "",
+                                  data$Country_Region,
+                                  data$Province_State)
+View(data)
+
+
+
